28ead930a4452be04a3e481eb504d7b0e796cd11
ooviya-M <113492572+ooviya-M@users.noreply.github.com>
2022-09-13 23:40:28 -0700

    Create Swap2NumsWithoutThirdVariable.java

---
+++ Swap2NumsWithoutThirdVariable.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println("Before Swapping: "+a+" "+b);
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.print("After Swapping: "+a+" "+b);
+    }
+}
