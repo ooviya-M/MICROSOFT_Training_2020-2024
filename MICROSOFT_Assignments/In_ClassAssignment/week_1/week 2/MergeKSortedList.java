0a1d214977de1af7ea35a68495c36d554cc071db
ooviya-M <113492572+ooviya-M@users.noreply.github.com>
2022-10-26 14:06:56 +0530

    Create MergeKSortedList.java

---
+++ MICROSOFT_Assignments/In_ClassAssignment/week_1/week 2/MergeKSortedList.java
@@ -0,0 +1,31 @@
+class Solution {
+    public ListNode merge(ListNode l1,ListNode l2){
+        if(l1==null){
+            return l2;
+            
+        }if(l2==null){
+            return l1;
+        }
+        if(l1.val<l2.val){
+            l1.next=merge(l1.next,l2);
+            return l1;
+        }else{
+            l2.next=merge(l1,l2.next);
+            return l2;
+        }
+    }
+    public ListNode mergeKLists(ListNode[] lists) {
+       if(lists.length==0){
+           return null;
+       }
+       int interval=1;
+        while(interval<lists.length){
+            for(int i=0;i+interval<lists.length;i=i+interval*2){
+            lists[i]=merge(lists[i],lists[i+interval]);
+            }    
+        
+        interval*=2;
+        }
+        return lists[0];
+    }
+}
