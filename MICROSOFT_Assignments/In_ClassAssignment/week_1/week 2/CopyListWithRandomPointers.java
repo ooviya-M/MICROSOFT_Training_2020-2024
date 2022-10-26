a876fcdc6738400e7037a5d9895544c83cb61ff4
ooviya-M <113492572+ooviya-M@users.noreply.github.com>
2022-10-26 14:04:59 +0530

    Create CopyListWithRandomPointers.java

---
+++ MICROSOFT_Assignments/In_ClassAssignment/week_1/week 2/CopyListWithRandomPointers.java
@@ -0,0 +1,17 @@
+class Solution {
+    public Node copyRandomList(Node head) {
+        Map<Node,Node> map=new HashMap<>();
+        Node node=head;
+        while(node!=null){
+            map.put(node,new Node(node.val));
+            node=node.next;
+        }
+        node=head;
+        while(node!=null){
+            map.get(node).next=map.get(node.next);
+            map.get(node).random=map.get(node.random);
+            node=node.next;
+        }
+        return map.get(head);
+    }
+}
