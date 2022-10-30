5df4586441361c1ebd333fd5091321295d0b0cb2
ooviya-M <113492572+ooviya-M@users.noreply.github.com>
2022-10-30 21:42:17 +0530

    Create Balanced Binary Tree.java

---
+++ MICROSOFT_Assignments/In_ClassAssignment/week_1/Balanced Binary Tree.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if(root==null){
+            return true;
+        }
+        return height(root)!=-1;
+    }
+    public int height(TreeNode node){
+        if(node==null){
+            return 0;
+        }
+        int lh=height(node.left);
+        if(lh==-1){
+            return -1;
+        }
+        int rh=height(node.right);
+        if(rh==-1){
+            return -1;
+        }
+        if(lh-rh<-1 || lh-rh>1){
+            return -1;
+        }
+        return Math.max(rh,lh)+1;
+    }
+}
