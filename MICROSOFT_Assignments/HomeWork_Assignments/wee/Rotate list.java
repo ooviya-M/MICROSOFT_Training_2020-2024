4ff010a58297b3353c4a3b9dd112572956979c45
ooviya-M <113492572+ooviya-M@users.noreply.github.com>
2022-10-30 22:14:38 +0530

    Create Rotate list.java

---
+++ MICROSOFT_Assignments/HomeWork_Assignments/wee/Rotate list.java
@@ -0,0 +1,23 @@
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head==null || k==0){
+            return head;
+        }
+        int length=0;
+        ListNode tail=head;
+        while(tail.next!=null){
+            length++;
+            tail=tail.next;
+        }
+        length++;
+        tail.next=head;
+        tail=head;
+        int rotatePoint=length-(k%length);
+        while(rotatePoint-- >1){
+            tail=tail.next;
+        }
+        head=tail.next;
+        tail.next=null;
+        return head;
+    }
+}
